c94969e1d0390bd589ac1229375bf69e1e399dd8
zinni ratul aini <zinni.alfarih30@gmail.com>
2020-11-20 10:36:12 +0800

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/simple/example/hewanpedia/model/Air.java
@@ -0,0 +1,8 @@
+package simple.example.hewanpedia.model;
+
+public class Air  extends Hewan{
+    public Air(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kelinci",ras,asal,deskripsi,drawableRes);
+
+    }
+}
---
+++ app/src/main/java/simple/example/hewanpedia/model/Ikan.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Ikan extends Hewan{
+
+    public Ikan(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Ikan",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
